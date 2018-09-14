05fa2b68829a611740d3dc2fc0593d57e2ab30c5
Madison <Madison@DESKTOP-P5AOLF9>
2018-09-14 17:39:18 -0400

    Created registers

--- Chip8/Memory/Memory.cs
+++ Chip8/Memory/Memory.cs
@@ -8,16 +8,16 @@ namespace Chip8.Memory
 {
     public class Memory : IMemory
     {
-        byte[] array = new byte[0x1000];
+        byte[] memory = new byte[0x1000];
 
         public byte ReadByte(ushort address)
         {
-            return array[address];
+            return memory[address];
         }
 
         public void WriteByte(ushort address, byte data)
         {
-            array[address] = data;
+            memory[address] = data;
         }
     }
 }
---
+++ Chip8/Registers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8
+{
+    public class Registers
+    {
+        public byte Delay = 0;
+        public byte Sound = 0;
+        public ushort I = 0;
+        public byte[] V = new byte[0x10];
+    }
+}
